c94f663590fa48da0643c2cb0ee54d47059b5e17
kyo <r.kyo0821@icloud.com>
2019-05-15 10:35:59 +0000

    favorites

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,11 +39,16 @@ class UsersController < ApplicationController
     @followers = @user.followers.page(params[:page])
     counts(@user)
   end
+  
+  def likes
+    @user = User.find(params[:id])
+    @like_microposts = @user.like_microposts.page(params[:page])
+    counts(@user)
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-
 end
